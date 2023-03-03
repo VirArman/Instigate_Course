433532daa92cbc548c1263b443d2331a9936c925
Arman <armanvirabyan15@gmail.com>
2023-03-03 10:58:50 +0400

    fixing bugs of longest and shortest words task

--- cpp_hw/strings.cpp
+++ cpp_hw/strings.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <string>
 std::string get_longest(std::string);
 std::string get_shortest(std::string);
 //std::string mytrim(std::string);
@@ -6,21 +7,29 @@ std::string get_shortest(std::string);
 //std::string back_trim(std::string);
 int main()
 {
-    std::string mstr;
+    std::string mstr, same = "";
     std::cout<<"Insert sentence\n";
     std::getline(std::cin,mstr);
 //    mstr = mytrim(mstr);
     std::string min = get_shortest(mstr);
     std::string max = get_longest(mstr);
+    for(int i = 0; i < min.size(); i++)
+    {
+        for(int j = 0; j<max.size(); j++)
+        {
+            if(min[i] == max[j]) same += min[i];
+        }
+    }
     std::cout<<"The longest word in sentence is "<<max<<std::endl;
     std::cout<<"The shortest word in sentence is "<<min<<std::endl;
+    std::cout<<"The same letters used in longest and shortest words "<<same<<std::endl;
 }
 
 std::string get_longest(std::string str)
 {
     std::string max = "";
     std::string temp = ""; 
-    for(int i = 0; str[i] != '\0'; i++)
+    for(int i = 0; i < str.size(); i++)
     {
         if(str[i] == ' ') temp = "";
         else temp += str[i];
@@ -34,20 +43,22 @@ std::string get_shortest(std::string str)
 {
     std::string min;
     std::string temp = "";
-    bool flag = true;
-    for(int i = 0; str[i] != '\0'; i++)
-    {
+    bool first_word = true;
+    for(int i = 0; i < str.size(); i++)
+    {   
+        bool new_word = false;
         if(str[i] == ' ')
         {
-            if(flag)
+            if(first_word)
             {
                 min = temp;
-                flag = false;
+                first_word = false;
             }
-            if(temp.size() < min.size()) 
-                min = temp;
-            temp = "";
-        } else  temp += str[i];
+            new_word = true;
+        }else  temp += str[i];
+        if(i == str.size()-1) new_word = true;
+        if(new_word && temp.size() < min.size()) min = temp;
+        if(new_word) temp = "";
     }
     return min;
 }
