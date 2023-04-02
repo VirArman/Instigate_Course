8eb39349b2b3bc6107eb96ab9871bb6b75787111
Arman <armanvirabyan15@gmail.com>
2023-04-02 17:33:23 +0400

    Fixing bugs

--- cpp_hw/test/main.cpp
+++ cpp_hw/test/main.cpp
@@ -5,6 +5,7 @@ int digit_sum(int num);
 int inverse_num(int num);
 bool isfloating(std::string line);
 int getting_digits(std::string line);
+bool non_digit(std::string line);
 
 int main(int argc, char* argv[])
 {
@@ -29,29 +30,49 @@ int main(int argc, char* argv[])
     }//checking the existance of testing flag
     else if(argv[1][0] == '-' && argv[1][1] == 't')
     {//testing mod
-        std::string input = argv[2];
-        std::string output = argv[3];
-        std::ifstream input_file(input);
-        std::ofstream output_file(output);
-        int num;
-        std::string line;
-        while (getline (input_file, line)) 
-        {//Reading from file and writeing results in output file 
-            try
+        std::ofstream output_file(argv[3]);
+        output_file.clear();
+        try
+        {
+            std::ifstream input_file(argv[2]);
+            if(!input_file)
             {
-                if(isfloating(line))
-                    throw 1;
-                num = getting_digits(line);
-                output_file << digit_sum(num) << "\n";
+                std::cout << "Input file does not exist" << "\n";
+                throw 1;
+            } 
+            int num;
+            std::string line;
+            while (getline (input_file, line)) 
+            {//Reading from file and writeing results in output file 
+                try
+                {
+                    if(isfloating(line))
+                        throw 2;
+                    if(non_digit(line))
+                    {
+                        throw 3;
+                    }
+                    num = getting_digits(line);
+                    output_file << digit_sum(num) << "\n";
+                }
+                catch(int err)
+                {
+                    if(err == 2)
+                        output_file << "The program works only with integers" << "\n";
+                    if(err == 3)
+                        output_file << "The line must contain only digits" << "\n";
+                }
             }
-            catch(int err)
-            {
-                if(err == 1)
-                    output_file << "The program works only with integers" << "\n";
-            }
+            output_file.close();
+            input_file.close();
         }
-        output_file.close();
-        input_file.close();
+        catch(int err)
+        {
+            if(err == 1)
+                output_file << "Input file does not exist" << "\n";
+            
+        }
+        
     }else
     {
         std::cout << "Entered flag does not exist " << "\n";
@@ -70,6 +91,18 @@ bool isfloating(std::string line)
     }
     return false;
 }
+bool non_digit(std::string line)
+{//Chekcs the line. If it has char that does not represent number it returns true
+    bool res = true;
+    for(int i = 0; i < line.size() ; i++)
+    {
+        if(isdigit(line[i]) || line[i] == '-')
+            res = false;
+        else
+            return true;
+    }
+    return res;
+}
 int getting_digits(std::string line)
 {//finds digits from line, concatinates them and creates integer
     int num = 0;
@@ -84,6 +117,7 @@ int getting_digits(std::string line)
     if(line[0] == '-') num *= -1;
     return num;
 }
+
 int digit_sum(int num)
 {//counts sum of digits
     int sum = 0;
--- cpp_hw/test/test.sh
+++ cpp_hw/test/test.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
-./a.out -t input.txt output.txt
 
-if cmp -s output.txt golden.txt; then
-  echo "The content of output and golden is identical"
+# Check if two file names were provided
+if [ "$#" -ne 2 ]; then
+  echo "Usage: $0 <file1> <file2>"
+  exit 1
+fi
+
+# Check if both files exist
+if [ ! -f "$1" ] || [ ! -f "$2" ]; then
+  echo "Error: Both files must exist"
+  exit 1
+fi
+g++ main.cpp
+./a.out -t "$1" "$2"
+
+if cmp -s $2 golden.txt; then
+  echo "All tests passed"
 else
-  diff golden.txt output.txt 
+  diff golden.txt $2 
 fi
