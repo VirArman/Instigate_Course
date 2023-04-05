2ee465c5e7471bc94e5e09f5f5d290f8389c591f
Arman <armanvirabyan15@gmail.com>
2023-04-05 16:27:26 +0400

    adding coments

--- cpp_hw/oop/smart_array.hpp
+++ cpp_hw/oop/smart_array.hpp
@@ -3,16 +3,27 @@ class Arr {
         int* m_arr = nullptr;
         int m_size = 0;
         int m_capacity = 4;
+        //changes capacity of array
         int* change_capacity();
     public:
+        //Constructor without argument
         Arr();
+        //Constructor with size argument
         Arr(int s);
+        //Constructor with size and array argument
         Arr(int s, int a[]);
+        //Destructor
         ~Arr();
+        //[] operator overloading
         int& operator[](int index);
+        //returns size
         int len();
+        //pushes value at the end of array
         void push(int num);
+        //adds value in specefic index
         void insert(int num, int index);
+        //deletes element from specified index
         void del(int index);
+        //prints defined elements of array
         void print();
 };
